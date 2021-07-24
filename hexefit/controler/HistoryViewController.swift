9c9ea48a5d75cc96a8db7b3fd7dcb97f610e59db
renatarego <renata.lucia@gmail.com>
2021-07-24 11:15:11 +0200

    added dispatch group to load workouts.

--- hexefit/controler/HistoryViewController.swift
+++ hexefit/controler/HistoryViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-import HealthKit
+
+
 
 class HistoryViewController: UIViewController {
     
     var userId: String?
-    var healthStore: HKHealthStore?
     var hexWorkouts: [HexWorkout]?
     
     var hkAssistant = HealthKitAssistant()
@@ -21,56 +21,49 @@ class HistoryViewController: UIViewController {
     
     // MARK: - HealthKit Methods
     
-    
-    
-    
-    // MARK: - LifeCycle Methods
-    
-    override  func viewDidAppear(_ animated: Bool) {
-        print("viewDidAppear")
-        tableView.dataSource = self
-        hexWorkouts = hexWorkouts?.sorted(by: { ($0.hkWorkout.endDate) > ($1.hkWorkout.endDate) })
-        //        print(hexWorkouts)
-    }
-    
-    override  func viewWillAppear(_ animated: Bool) {
-        print("viewWillAppear")
-        //healthStoreAvailable = createHealthStore()
-        
+    func loadWorkouts(){
+//        var count = 1
         hkAssistant.loadWorkouts { (workouts, error) in
             print("N. of workouts: \(workouts!.count)")
             if error == nil{
                 self.hexWorkouts = []
                 guard self.hexWorkouts != nil else {fatalError("HexWorkouts is nill")}
+                
+                let group = DispatchGroup()
                 for workout in workouts!{
+                    group.enter()
+//                    print(count)
+//                    count += 1
+                    
                     self.hkAssistant.loadHeartRates(for: workout) { samples, error in
+                        group.leave()
                         if error == nil{
                             let hexWorkout = HexWorkout(hkWorkout: workout, heartRates: samples!)
                             self.hexWorkouts!.append(hexWorkout)
                         }
                     }
                 }
-                DispatchQueue.main.async {
-                      self.tableView.reloadData()
+                
+     
+//                self.hexWorkouts = self.hexWorkouts?.sorted(by: { ($0.hkWorkout.endDate) > ($1.hkWorkout.endDate) })
+                group.notify(queue: .main) {
+                    print("N. of hexworkouts: \(self.hexWorkouts!.count)")
+                    DispatchQueue.main.async {
+                          self.tableView.reloadData()
                     }
+                }
+               
+
             }
             
             else{
                 print("load workouts produced an error")
             }
-            DispatchQueue.main.async {
-                  self.tableView.reloadData()
-                }
+ 
         }
-        
-        
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        
+    func authorizeHealthKit(){
         HealthKitAssistant.authorizeHealthKit { (authorized, error) in
             
             guard authorized else {
@@ -82,22 +75,39 @@ class HistoryViewController: UIViewController {
                 } else {
                     print(baseMessage)
                 }
-                DispatchQueue.main.async {
-                      self.tableView.reloadData()
-                    }
+                
+                
                 
                 return
             }
             
+            self.loadWorkouts()
+            DispatchQueue.main.async {
+                  self.tableView.reloadData()
+            }
             print("HealthKit Successfully Authorized.")
         }
+    }
         
+    
+    
+    
+    // MARK: - LifeCycle Methods
+    
 
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        
+        
         tableView.register(UINib(nibName: "HistoryCell", bundle: nil), forCellReuseIdentifier: "historyReusableCell")
         tableView.rowHeight = 120;
         tableView.separatorStyle = .none
         
-//        tableView.dataSource = self
+        authorizeHealthKit()
+        
+        
         
     }
     
@@ -150,97 +160,3 @@ extension HistoryViewController: UITableViewDataSource{
     
 }
 
-extension HKWorkoutActivityType {
-    
-    /*
-     Simple mapping of available workout types to a human readable name.
-     */
-    var name: String {
-        switch self {
-        case .americanFootball:             return "American Football"
-        case .archery:                      return "Archery"
-        case .australianFootball:           return "Australian Football"
-        case .badminton:                    return "Badminton"
-        case .baseball:                     return "Baseball"
-        case .basketball:                   return "Basketball"
-        case .bowling:                      return "Bowling"
-        case .boxing:                       return "Boxing"
-        case .climbing:                     return "Climbing"
-        case .crossTraining:                return "Cross Training"
-        case .curling:                      return "Curling"
-        case .cycling:                      return "Cycling"
-        case .dance:                        return "Dance"
-        case .danceInspiredTraining:        return "Dance Inspired Training"
-        case .elliptical:                   return "Elliptical"
-        case .equestrianSports:             return "Equestrian Sports"
-        case .fencing:                      return "Fencing"
-        case .fishing:                      return "Fishing"
-        case .functionalStrengthTraining:   return "Functional Strength Training"
-        case .golf:                         return "Golf"
-        case .gymnastics:                   return "Gymnastics"
-        case .handball:                     return "Handball"
-        case .hiking:                       return "Hiking"
-        case .hockey:                       return "Hockey"
-        case .hunting:                      return "Hunting"
-        case .lacrosse:                     return "Lacrosse"
-        case .martialArts:                  return "Martial Arts"
-        case .mindAndBody:                  return "Mind and Body"
-        case .mixedMetabolicCardioTraining: return "Mixed Metabolic Cardio Training"
-        case .paddleSports:                 return "Paddle Sports"
-        case .play:                         return "Play"
-        case .preparationAndRecovery:       return "Preparation and Recovery"
-        case .racquetball:                  return "Racquetball"
-        case .rowing:                       return "Rowing"
-        case .rugby:                        return "Rugby"
-        case .running:                      return "Running"
-        case .sailing:                      return "Sailing"
-        case .skatingSports:                return "Skating Sports"
-        case .snowSports:                   return "Snow Sports"
-        case .soccer:                       return "Soccer"
-        case .softball:                     return "Softball"
-        case .squash:                       return "Squash"
-        case .stairClimbing:                return "Stair Climbing"
-        case .surfingSports:                return "Surfing Sports"
-        case .swimming:                     return "Swimming"
-        case .tableTennis:                  return "Table Tennis"
-        case .tennis:                       return "Tennis"
-        case .trackAndField:                return "Track and Field"
-        case .traditionalStrengthTraining:  return "Traditional Strength Training"
-        case .volleyball:                   return "Volleyball"
-        case .walking:                      return "Walking"
-        case .waterFitness:                 return "Water Fitness"
-        case .waterPolo:                    return "Water Polo"
-        case .waterSports:                  return "Water Sports"
-        case .wrestling:                    return "Wrestling"
-        case .yoga:                         return "Yoga"
-            
-        // iOS 10
-        case .barre:                        return "Barre"
-        case .coreTraining:                 return "Core Training"
-        case .crossCountrySkiing:           return "Cross Country Skiing"
-        case .downhillSkiing:               return "Downhill Skiing"
-        case .flexibility:                  return "Flexibility"
-        case .highIntensityIntervalTraining:    return "High Intensity Interval Training"
-        case .jumpRope:                     return "Jump Rope"
-        case .kickboxing:                   return "Kickboxing"
-        case .pilates:                      return "Pilates"
-        case .snowboarding:                 return "Snowboarding"
-        case .stairs:                       return "Stairs"
-        case .stepTraining:                 return "Step Training"
-        case .wheelchairWalkPace:           return "Wheelchair Walk Pace"
-        case .wheelchairRunPace:            return "Wheelchair Run Pace"
-            
-        // iOS 11
-        case .taiChi:                       return "Tai Chi"
-        case .mixedCardio:                  return "Mixed Cardio"
-        case .handCycling:                  return "Hand Cycling"
-            
-        // iOS 13
-        case .discSports:                   return "Disc Sports"
-        case .fitnessGaming:                return "Fitness Gaming"
-            
-        // Catch-all
-        default:                            return "Other"
-        }
-    }
-}
